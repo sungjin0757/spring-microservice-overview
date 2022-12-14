782a7bb9faaaee6d7550ea89a3af8b78648a7b26
sungjin <sungjin0757@naver.com>
2022-12-14 11:43:37 +0900

    Add Docker File

---
+++ licensing-service/docker/run.sh
@@ -0,0 +1,4 @@
+echo "********************************************************"
+echo "Starting License Server"
+echo "********************************************************"
+java -jar /usr/local/licensing-service/app.jar
